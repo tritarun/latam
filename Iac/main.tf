b234c90b5495db1490ddb4c7fbb00435fb454083
Tarun Kumar Gupta <tarun.gupta@globant.com>
2024-05-05 18:03:05 -0400

    trying fist code for cloud storage

--- Iac/main.tf
+++ Iac/main.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "test-storage-bucket" {
   name          = "watchful-pier-422221-q7_storage_bucket"
   location      = "US"
   force_destroy = true
+  project = "Active project"
 
   public_access_prevention = "enforced"
 }
