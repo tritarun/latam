ba8facd270c1bf5b9a5683e09e09ca05d121f0fa
Tarun Kumar Gupta <tarun.gupta@globant.com>
2024-05-05 16:19:20 -0400

    test commit

---
+++ Iac/main.tf
@@ -0,0 +1,7 @@
+resource "google_storage_bucket" "test-storage-bucket" {
+  name          = "watchful-pier-422221-q7_storage_bucket"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
