d3532ab0bdc4c18634309ce45c1e113c53548a83
Tarun Kumar Gupta <tarun.gupta@globant.com>
2024-05-06 15:45:59 -0400

    removing the test storage bucket

--- Iac/BD/main.tf
+++ Iac/BD/main.tf
@@ -1,8 +1,8 @@
-resource "google_storage_bucket" "test-storage-bucket" {
-  name          = "watchful-pier-422221-q7_storage_bucket"
-  location      = "US"
-  force_destroy = true
-  project     = "watchful-pier-422221-q7"
+# resource "google_storage_bucket" "test-storage-bucket" {
+#   name          = "watchful-pier-422221-q7_storage_bucket"
+#   location      = "US"
+#   force_destroy = true
+#   project     = "watchful-pier-422221-q7"
 
-  public_access_prevention = "enforced"
-}
+#   public_access_prevention = "enforced"
+# }
