86e7f9e7525d13cb3d2c3a676633e65bf9cc4aea
Tarun Kumar Gupta <tarun.gupta@globant.com>
2024-05-06 14:59:27 -0400

    change in terraform.yml file

--- Iac/BD/main.tf
+++ Iac/BD/main.tf
@@ -2,7 +2,6 @@ resource "google_storage_bucket" "test-storage-bucket" {
   name          = "watchful-pier-422221-q7_storage_bucket"
   location      = "US"
   force_destroy = true
-  project = "Active project"
 
   public_access_prevention = "enforced"
 }
