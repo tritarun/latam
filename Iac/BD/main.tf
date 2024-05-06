3463a7a6ef3b39b958944f32c1b5c2c1a124b6a9
Tarun Kumar Gupta <tarun.gupta@globant.com>
2024-05-06 15:25:14 -0400

    change in terraform installation dir

--- Iac/BD/main.tf
+++ Iac/BD/main.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "test-storage-bucket" {
   name          = "watchful-pier-422221-q7_storage_bucket"
   location      = "US"
   force_destroy = true
+  project     = "watchful-pier-422221-q7"
 
   public_access_prevention = "enforced"
 }
